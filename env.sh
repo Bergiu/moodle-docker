ce016f46d97027fbdaeda862e84f2ab0880013f6
Marco Schlicht <schlicht@itc.rwth-aachen.de>
2018-11-23 13:25:39 +0100

    added environment

---
+++ env.sh
@@ -0,0 +1,4 @@
+# Set up path to Moodle code
+export MOODLE_DOCKER_WWWROOT=/srv/http/
+# Choose a db server (Currently supported: pgsql, mariadb, mysql, mssql, oracle)
+export MOODLE_DOCKER_DB=mysql
